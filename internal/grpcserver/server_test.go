7e93eab98546d75afa3de7a44f1be71261c27b67
Casey Brooks <casey.brooks@agyn.io>
2026-03-17 04:31:59 +0000

    feat: align files service tooling

--- internal/grpcserver/server_test.go
+++ internal/grpcserver/server_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 	"time"
 
-	filesv1 "github.com/agynio/files/gen/go/agynio/api/files/v1"
+	filesv1 "github.com/agynio/files/.gen/go/agynio/api/files/v1"
 	"github.com/agynio/files/internal/filestore"
 	"github.com/agynio/files/internal/model"
 	"github.com/google/uuid"
